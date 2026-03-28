86ce1705bdbd7692e3df4699e08780ca1780ed9e
senura-sandeepa <prabashwara13@gmail.com>
2026-03-28 23:34:42 +0530

    Fix delete item foreign key constraint

--- src/main/java/com/senura/smart_grocery_finder/service/ItemService.java
+++ src/main/java/com/senura/smart_grocery_finder/service/ItemService.java
@@ -2,8 +2,10 @@ package com.senura.smart_grocery_finder.service;
 
 import com.senura.smart_grocery_finder.dto.*;
 import com.senura.smart_grocery_finder.entity.Item;
+import com.senura.smart_grocery_finder.entity.Shop;
 import com.senura.smart_grocery_finder.exception.ResourceNotFoundException;
 import com.senura.smart_grocery_finder.repository.ItemRepository;
+import com.senura.smart_grocery_finder.repository.ShopRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,7 @@ import java.util.stream.Collectors;
 public class ItemService {
 
     private final ItemRepository itemRepository;
+    private final ShopRepository shopRepository;
 
     // Create a new item
     public ItemResponse createItem(ItemRequest request) {
@@ -33,10 +36,19 @@ public class ItemService {
                 .collect(Collectors.toList());
     }
 
+
     // Delete item by id
     public void deleteItem(Long id) {
         Item item = itemRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Item not found with id: " + id));
+
+        // Remove item from all shops first
+        List<Shop> shops = shopRepository.findAll();
+        for (Shop shop : shops) {
+            shop.getItems().remove(item);
+            shopRepository.save(shop);
+        }
+
         itemRepository.delete(item);
     }
 
