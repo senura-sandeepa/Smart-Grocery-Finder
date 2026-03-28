0157e0327fa14e1810f8dcd15a85e1cb3894ef11
senura-sandeepa <prabashwara13@gmail.com>
2026-03-28 21:33:59 +0530

    Fix coordinate serialization in ShopRequest and ShopResponse

--- src/main/java/com/senura/smart_grocery_finder/dto/ShopRequest.java
+++ src/main/java/com/senura/smart_grocery_finder/dto/ShopRequest.java
@@ -1,12 +1,17 @@
 package com.senura.smart_grocery_finder.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class ShopRequest {
 
     private String name;
+
+    @JsonProperty("xCoordinate")
     private double xCoordinate;
+
+    @JsonProperty("yCoordinate")
     private double yCoordinate;
 
 }
